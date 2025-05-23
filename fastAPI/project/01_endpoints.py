ab7314796c598d204acf85249a5235a43ab45b48
Valentin <jkv21contact@gmail.com>
2025-05-23 10:24:21 +0200

    add endpoint documentation and clean code

--- fastAPI/project/01_endpoints.py
+++ fastAPI/project/01_endpoints.py
@@ -1,20 +1,15 @@
-# import fastapi 
-
 from fastapi import FastAPI
 
-# create API object
 app = FastAPI()
 
-# Get first endpoint/ route
-# /hello    /get-item
 @app.get("/")
-# Function that would be called when the endpoint is hit
-# it doesn't matter the name of the function as long as it's
-# right under it gets called
 def read_root():
-    return {"Hello": "World, I'm testing my FastAPI"}
-  
-@app.get('/about')
-def about():
-    return {"Description": "This is a test API for learning FastAPI. Welcome to the About Page!"}
+    return {"message": "Hello, world! I'm testing my FastAPI."}
+
+@app.get("/about")
+def get_about():
+    return {
+        "description": "This is a test API for learning FastAPI. Welcome to the About Page!"
+    }
+
 
